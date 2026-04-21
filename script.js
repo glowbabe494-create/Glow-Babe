c3e107ef5cda6490899d84f71167a765f9b248b4
glowbabe494-create <glowbabe494@gmail.com>
2026-04-21 13:48:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function sendOrder() {
-  let orderText = document.getElementById("orderDetails").value;
-  let whatsappUrl = "https://wa.me/923146604294?text=" + encodeURIComponent(orderText);
+  let email = document.getElementById("email").value;
+  let newsletter = document.getElementById("newsletter").checked ? "Yes" : "No";
+  let fname = document.getElementById("fname").value;
+  let lname = document.getElementById("lname").value;
+  let address = document.getElementById("address").value;
+  let city = document.getElementById("city").value;
+  let postal = document.getElementById("postal").value;
+  let phone = document.getElementById("phone").value;
+
+  let orderText =
+    "New Order Details:%0A" +
+    "Email: " + email + "%0A" +
+    "Newsletter: " + newsletter + "%0A" +
+    "Name: " + fname + " " + lname + "%0A" +
+    "Address: " + address + ", " + city + "%0A" +
+    "Postal: " + postal + "%0A" +
+    "Phone: " + phone;
+
+  let whatsappUrl = "https://wa.me/923146604294?text=" + orderText;
   window.open(whatsappUrl, "_blank");
 }
